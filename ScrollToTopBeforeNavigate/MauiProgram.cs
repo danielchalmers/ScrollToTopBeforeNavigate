60f4919425e6647ea59f1b1279ce3d4d42517089
Daniel Chalmers <daniel.chalmers@outlook.com>
2024-02-08 13:54:30 -0600

    Revert "mudblazor test"
    This reverts commit 5a51002a3b4c3650f1024392a2e5becfe5a21536.

--- ScrollToTopBeforeNavigate/MauiProgram.cs
+++ ScrollToTopBeforeNavigate/MauiProgram.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.Extensions.Logging;
-using MudBlazor.Services;
 
 namespace ScrollToTopBeforeNavigate;
 public static class MauiProgram
@@ -14,7 +13,7 @@ public static class MauiProgram
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 			});
 
-		builder.Services.AddMauiBlazorWebView(); builder.Services.AddMudServices();
+		builder.Services.AddMauiBlazorWebView();
 
 #if DEBUG
 		builder.Services.AddBlazorWebViewDeveloperTools();
